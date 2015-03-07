a85ed711f8adcebdb57ba662812dc31a0728e9cb
Bob Whitelock <bob.whitelock1@gmail.com>
2015-03-07 12:24:19 +0000

    Create title class to parse out constituent parts of a title according to the usual structure.

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@
 # determine if api has up-to-date info - if not is this ok?
 # tidy up
 
+import re
 import time
 
 import praw
@@ -25,9 +26,80 @@ BAND_SEARCH_API_END_POINT = METAL_ARCHIVES_API_URL + '/api/bands/name/'
 
     # time.sleep(60)
 
+
+class Title:
+
+    SEPARATOR = '-'
+    NON_BRACKETED_REGEX = '[^\[\]\(\)]+'
+    CIRCLE_BRACKETS_REGEX = '\(([^\(\)]*)\)'
+    SQUARE_BRACKETS_REGEX = '\[([^\[\]]*)\]'
+
+    def __init__(self, title_text):
+        """ Process text of a title to try and determine major features.
+
+            Titles usually tend to conform to following structure:
+            band_name <always> - song_name <always> [genre <often>] (label <often>) further_description <sometimes>
+            The band name and song name almost always appear and in this
+            format but other parts sometimes don't appear or are in different
+            orders.
+        """
+        # split title along separator
+        title_parts = title_text.split(Title.SEPARATOR, 1)
+        if len(title_parts) == 2:
+            # parse out parts of title in turn as much as possible
+            self.band = title_parts[0].strip()
+            self._title_remainder = title_parts[1].strip()
+
+            self._parse_song_name()
+            self._parse_genre()
+            self._parse_label()
+
+            self.further_description = self._title_remainder
+        else:
+            # if title not split along separator then does not conform to
+            # format so can't parse, so set all fields to None
+            self.band = None
+            self.song = None
+            self.genre = None
+            self.label = None
+            self.further_description = None
+
+    def _parse_song_name(self):
+        """ Parse song as everything in title remainder up to first brackets, if this exists. """
+        song_name_match = re.match(Title.NON_BRACKETED_REGEX, self._title_remainder)
+        if song_name_match:
+            self.song = song_name_match.group().strip()
+            # set remainder to be everything after match
+            self._title_remainder = self._title_remainder[song_name_match.end():]
+        else:
+            self.song = None
+
+    def _parse_genre(self):
+        """ Parse genre as contents of first square brackets, if any exist. """
+        genre_match = re.search(Title.SQUARE_BRACKETS_REGEX, self._title_remainder)
+        if genre_match:
+            self.genre = genre_match.group(1).strip()
+            # set remainder to be everything after match
+            self._title_remainder = self._title_remainder[genre_match.end():]
+        else:
+            self.genre = None
+
+    def _parse_label(self):
+        """ Parse label as contents of first circle brackets, if any exist. """
+        label_match = re.search(Title.CIRCLE_BRACKETS_REGEX, self._title_remainder)
+        if label_match:
+            self.label = label_match.group(1).strip()
+            # set remainder to be existing remainder with match cut
+            # - sometimes info before and after circle brackets so include any of this
+            self._title_remainder = (self._title_remainder[:label_match.start()] + self._title_remainder[label_match.end():]).strip()
+        else:
+            self.label = None
+
+
+
 def identify(title):
     band_name_in_title = title.split('-')[0].strip()
-    band_search_url = BAND_SEARCH_API_END_POINT + band_name_in_title + '/'
+    band_search_url = BAND_SEARCH_API_END_POINT + band_name_in_title
     band_search_response = requests.get(band_search_url)
     potential_bands = band_search_response.json()
 
@@ -48,5 +120,15 @@ def identify(title):
     time.sleep(1)
 
 
+# def find_bands_with
+
+
 if __name__ == '__main__':
-    identify('Voices')
+    # identify('Voices')
+
+    t = Title('Cloud Rat - Blind River [Metallic Hardcore] (Halo of Flies)')
+    print(t.band)
+    print(t.song)
+    print(t.genre)
+    print(t.label)
+    print(t.further_description)
